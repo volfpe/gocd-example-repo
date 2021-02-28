57207294220f098dc202844e383785c94e1c8d6b
Petr Volf <petr.volf96@gmail.com>
2021-02-28 16:02:40 +0100

    x

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -4,4 +4,5 @@ echo "pod2 build"
 echo "second line"
 ls -la
 pwd
-/kaniko/executor --dockerfile $WORKSPACE/server.Dockerfile --no-push
+echo $WORKSPACE
+/kaniko/executor --context $WORKSPACE --dockerfile $WORKSPACE/server.Dockerfile --no-push
