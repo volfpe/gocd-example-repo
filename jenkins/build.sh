64feede78f1b3e440bd96c6d277f9d1cfe6cb463
Petr Volf <petr.volf96@gmail.com>
2021-03-01 23:44:23 +0100

    x

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -7,5 +7,5 @@ pwd
 echo $WORKSPACE
 echo ${DOCKER_REPOSITORY}server:${BUILD_ID}
 #docker login $DOCKER_HOST -u $DOCKER_USER -p $DOCKER_PASS
-docker build -f $WORKSPACE/server.Dockerfile -t ${DOCKER_HOST}/${DOCKER_PROJECT}/server:${BUILD_ID} $WORKSPACE
+docker build -f $WORKSPACE/server.Dockerfile $WORKSPACE
 #docker push "${DOCKER_HOST}/${DOCKER_PROJECT}/server:${BUILD_ID}"
