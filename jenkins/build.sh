874af88e8391d42358a82192b98ee2b49dc721bc
Petr Volf <petr.volf96@gmail.com>
2021-03-01 23:10:09 +0100

    x

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -5,4 +5,6 @@ echo "second line"
 ls -la
 pwd
 echo $WORKSPACE
-docker build -f $WORKSPACE/server.Dockerfile $WORKSPACE
+echo ${DOCKER_REPOSITORY}server:${BUILD_ID}
+docker build -f $WORKSPACE/server.Dockerfile -t "${DOCKER_REPOSITORY}server:${BUILD_ID}" $WORKSPACE
+docker push ${DOCKER_REPOSITORY}server:${BUILD_ID}
