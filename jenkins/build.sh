cc1d6df798d5f1d0fa642a33b03f7fa6d83499a8
Petr Volf <petr.volf96@gmail.com>
2021-03-01 23:26:04 +0100

    x

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -7,5 +7,5 @@ pwd
 echo $WORKSPACE
 echo ${DOCKER_REPOSITORY}server:${BUILD_ID}
 #docker login $DOCKER_HOST -u $DOCKER_USER -p $DOCKER_PASS
-docker build -f $WORKSPACE/server.Dockerfile -t "${DOCKER_HOST}/${DOCKER_PROJECT}/server:${BUILD_ID}" $WORKSPACE
+echo "docker build -f $WORKSPACE/server.Dockerfile -t ${DOCKER_HOST}/${DOCKER_PROJECT}/server:${BUILD_ID} $WORKSPACE" > /home/volfpe/debs.txt
 #docker push "${DOCKER_HOST}/${DOCKER_PROJECT}/server:${BUILD_ID}"
